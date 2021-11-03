6acc4966b41074a603a3fed1cdae5c4ab7fd2563
JE-Chen <zenxcvwait@gmail.com>
2021-11-03 23:44:39 +0800

    add exception tag

--- je_api_testka/utils/api_test_eceptions_tag.py
+++ je_api_testka/utils/api_test_eceptions_tag.py
@@ -1,5 +1,8 @@
+# Unknown error
 api_test_error_message = "API tester error"
+# get data error
 api_test_get_data_error_message = "API tester can't get api data error"
+# API Function error
 api_test_get_error_message = "API tester get error"
 api_test_get_json_error_message = "API tester get json error"
 api_test_put_error_message = "API tester put error"
@@ -9,3 +12,9 @@ api_test_head_error_message = "API tester head error"
 api_test_options_error_message = "API tester options error"
 api_test_patch_error_message = "API tester patch error"
 api_test_session_error_message = "API tester session error"
+# API response code error
+api_test_response_code_error = "API tester get wrong response code"
+# API response body error
+api_test_response_body_error = "API tester get wrong response body"
+# API response timeout error
+api_test_response_timeout_error = "API tester timeout"
