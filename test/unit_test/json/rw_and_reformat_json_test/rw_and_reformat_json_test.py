c7a788730006d4cb8dc05274604682d0c2a09ab3
JE-Chen <zenxcvwait@gmail.com>
2022-03-14 10:32:23 +0800

    fix an error
    fix an error

--- test/unit_test/json/rw_and_reformat_json_test/rw_and_reformat_json_test.py
+++ test/unit_test/json/rw_and_reformat_json_test/rw_and_reformat_json_test.py
@@ -6,13 +6,22 @@ from je_api_testka import write_action_json
 from je_api_testka import reformat_json
 
 
-test_list = [
-    ("get", "http://httpbin.org/get", False, {"headers": {
-        'x-requested-with': 'XMLHttpRequest',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
-    }}),
-    ("post", "http://httpbin.org/post", False, {"params": {"task": "new task"}})
+test_list = \
+[
+    ["test_api_method",
+     {"http_method": "get", "test_url": "http://httpbin.org/get",
+      "headers": {
+          'x-requested-with': 'XMLHttpRequest',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
+      }
+      }
+     ],
+    ["test_api_method",
+     {"http_method": "post", "test_url": "http://httpbin.org/post", "params": {"task": "new task"},
+      "result_check_dict": {"status_code": 200}
+      }
+     ]
 ]
 test_dumps_json = json.dumps(test_list)
 print(test_dumps_json)
