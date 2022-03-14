b0d39c9c7b20a83d2de9d741b0f25aeae446b2b4
JE-Chen <zenxcvwait@gmail.com>
2022-03-14 10:30:19 +0800

    Update argparse_test.py

--- test/unit_test/argparse/argparse_test.py
+++ test/unit_test/argparse/argparse_test.py
@@ -3,6 +3,6 @@ import os
 print(os.getcwd())
 
 os.system("cd " + os.getcwd())
-os.system("python je_api_testka" + r"/test/unit_test/argparse/test.json")
+os.system("python je_api_testka " + os.getcwd() + r"/test/unit_test/argparse/test.json")
 
 
