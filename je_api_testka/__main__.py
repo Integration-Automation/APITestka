115065ef47bbfecb8ad3a55ed8cf08ffc4d8d746
JE-Chen <zenxcvwait@gmail.com>
2022-04-05 23:58:27 +0800

    edit __main__
    edit __main__

--- je_api_testka/__main__.py
+++ je_api_testka/__main__.py
@@ -6,7 +6,7 @@ from je_api_testka.utils.executor.action_executor import execute_action
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("execute_file", type=str, help="choose action file to execute")
+    parser.add_argument("-e", "--execute_file", type=str, help="choose action file to execute")
     args = parser.parse_args()
     execute_action(read_action_json(args.execute_file))
     print('execute action:', args.execute_file)
