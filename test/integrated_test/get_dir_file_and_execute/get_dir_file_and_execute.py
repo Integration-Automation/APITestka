e0e8e355642a5ee7920585513ccadfc43ff8b4df
JE-Chen <zenxcvwait@gmail.com>
2022-04-07 02:18:51 +0800

    edit test
    edit test

--- test/integrated_test/get_dir_file_and_execute/get_dir_file_and_execute.py
+++ test/integrated_test/get_dir_file_and_execute/get_dir_file_and_execute.py
@@ -4,5 +4,6 @@ from je_api_testka import get_dir_files_as_list
 from je_api_testka import execute_files
 
 files_list = get_dir_files_as_list(os.getcwd())
+print(files_list)
 if files_list is not None:
     execute_files(files_list)
