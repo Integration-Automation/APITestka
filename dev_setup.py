8ef5dc1ca85ce3ff6c40d37fab968cedeccc683b
JE-Chen <zenxcvwait@gmail.com>
2022-04-07 03:07:15 +0800

    update dev version
    update dev version

--- dev_setup.py
+++ dev_setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as README:
 
 setuptools.setup(
     name="je_api_testka_dev",
-    version="0.0.25",
+    version="0.0.26",
     author="JE-Chen",
     author_email="zenmailman@gmail.com",
     description="api testing",
--- je_api_testka/utils/json/json_file/json_file.py
+++ je_api_testka/utils/json/json_file/json_file.py
@@ -16,7 +16,7 @@ def read_action_json(json_file_path: str):
     """
     try:
         lock.acquire()
-        file_path = Path(str(json_file_path).strip())
+        file_path = Path(json_file_path)
         if file_path.exists() and file_path.is_file():
             with open(json_file_path) as read_file:
                 return json.load(read_file)
