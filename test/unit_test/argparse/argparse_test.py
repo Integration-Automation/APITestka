6921e97e6c1f2b47e228bb120e7d8f9ede1a0d44
JE-Chen <zenxcvwait@gmail.com>
2022-04-06 10:12:23 +0800

    fix an test error
    fix an test error

--- test/unit_test/argparse/argparse_test.py
+++ test/unit_test/argparse/argparse_test.py
@@ -3,4 +3,4 @@ import os
 print(os.getcwd())
 
 os.system("cd " + os.getcwd())
-os.system("python -m je_api_testka --execute_file " + os.getcwd() + r"\test.json")
+os.system("python -m je_api_testka --execute_file " + os.getcwd() + r"/test/unit_test/argparse/test.json")
