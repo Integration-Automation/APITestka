df0b7532cb70d7418321cb0144d01c68428bb92b
JE-Chen <zenxcvwait@gmail.com>
2022-04-21 02:12:26 +0800

    update stable version
    update stable version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as README:
 
 setuptools.setup(
     name="je_api_testka",
-    version="0.0.60",
+    version="0.0.61",
     author="JE-Chen",
     author_email="zenmailman@gmail.com",
     description="api testing",
