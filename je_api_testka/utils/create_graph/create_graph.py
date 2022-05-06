11bdbc40b145bf49a2b69d1ab84439b81d11f0b1
JE-Chen <zenxcvwait@gmail.com>
2022-05-06 23:21:11 +0800

    update stable and dev version
    fix import error

--- je_api_testka/utils/create_graph/create_graph.py
+++ je_api_testka/utils/create_graph/create_graph.py
@@ -1,10 +1,5 @@
 from typing import Tuple
 
-try:
-    from je_matplotlib_wrapper import set_tkinter_embed_matplotlib_barh
-except ImportError as error:
-    raise ImportError(repr(error) + " you need install je_matplotlib_wrapper to use this")
-
 from tkinter import Tk
 
 from je_api_testka.utils.test_record.record_test_result_class import test_record
@@ -20,6 +15,10 @@ def get_list(get_data_horizontal: str, get_data_vertical: str) -> Tuple[list, li
 
 
 def make_tkinter_request_time_graph():
+    try:
+        from je_matplotlib_wrapper import set_tkinter_embed_matplotlib_barh
+    except ImportError as error:
+        raise ImportError(repr(error) + " you need install je_matplotlib_wrapper to use this")
     show_data = get_list("request_time_sec", "request_url")
     set_tkinter_embed_matplotlib_barh(
         y_content_list=show_data[0],
