1f1cfafd5e9796e8ec28878f8f18ab2cb75da6ba
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2022-10-27 17:16:01 +0800

    now api return data type not all str anymore
    now api return data type not all str anymore

--- je_api_testka/utils/get_data_strcture/get_api_data.py
+++ je_api_testka/utils/get_data_strcture/get_api_data.py
@@ -16,20 +16,20 @@ def get_api_response_data(response: Response,
     elapsed, request_time_sec, request_method, request_url, request_body, start_time, end_time]
     """
     response_data = {
-        "status_code": str(response.status_code),
-        "text": str(response.text).encode("utf-8"),
-        "content": str(response.content).encode("utf-8"),
-        "headers": str(response.headers).encode("utf-8"),
-        "history": str(response.history).encode("utf-8"),
-        "encoding": str(response.encoding).encode("utf-8"),
-        "cookies": str(dict_from_cookiejar(response.cookies)).encode("utf-8"),
-        "elapsed": str(response.elapsed).encode("utf-8"),
-        "request_time_sec": str(response.elapsed.total_seconds()).encode("utf-8"),
-        "request_method": str(response.request.method).encode("utf-8"),
-        "request_url": str(response.request.url).encode("utf-8"),
-        "request_body": str(response.request.body).encode("utf-8"),
-        "start_time": str(start_time).encode("utf-8"),
-        "end_time": str(end_time).encode("utf-8")
+        "status_code": response.status_code,
+        "text": response.text,
+        "content": response.content,
+        "headers": response.headers,
+        "history": response.history,
+        "encoding": response.encoding,
+        "cookies": dict_from_cookiejar(response.cookies),
+        "elapsed": response.elapsed,
+        "request_time_sec": response.elapsed.total_seconds(),
+        "request_method": response.request.method,
+        "request_url": response.request.url,
+        "request_body": response.request.body,
+        "start_time": start_time,
+        "end_time": end_time
     }
     try:
         if response_data.get("status_code") == 200:
