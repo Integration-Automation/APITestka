b19afd9023503cef57d67b5188aa5d7b02a2df13
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2022-11-01 20:09:03 +0800

    upload dev 0.58 ver and test
    now result check will return true or false

--- je_api_testka/utils/assert_result/result_check.py
+++ je_api_testka/utils/assert_result/result_check.py
@@ -5,12 +5,9 @@ def check_result(result_dict: dict, result_check_dict: dict):
     """
     :param result_dict: response result dict (get_api_response_data's return data)
     :param result_check_dict: the dict include data name and value to check result_dict is valid or not
-    :return:
+    :return: True if no error None if check failed
     """
     for key, value in result_check_dict.items():
         if result_dict.get(key) != value:
-            raise APIAssertException(
-                "value should be {right_value} but value was {wrong_value}".format(
-                    right_value=value, wrong_value=result_dict.get(key)
-                )
-            )
+            return False
+    return True
