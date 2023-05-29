dad30b0a55de465fc691665ae2e6dd319a10aa7b
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-05-29 11:25:44 +0800

    Update dev version
    Update dev version

--- je_api_testka/utils/executor/action_executor.py
+++ je_api_testka/utils/executor/action_executor.py
@@ -14,7 +14,6 @@ from je_api_testka.utils.generate_report.json_report import generate_json
 from je_api_testka.utils.generate_report.json_report import generate_json_report
 from je_api_testka.utils.generate_report.xml_report import generate_xml
 from je_api_testka.utils.generate_report.xml_report import generate_xml_report
-from je_api_testka.utils.global_dict.event_dict import event_dict
 from je_api_testka.utils.json.json_file.json_file import read_action_json
 from je_api_testka.utils.mock_server.flask_mock_server import flask_mock_server_instance
 from je_api_testka.utils.package_manager.package_manager_class import package_manager
@@ -23,7 +22,23 @@ from je_api_testka.utils.package_manager.package_manager_class import package_ma
 class Executor(object):
 
     def __init__(self):
-        self.event_dict = event_dict
+        self.event_dict = {
+            # test api
+            "test_api_method": test_api_method,
+            "generate_html": generate_html,
+            "generate_html_report": generate_html_report,
+            "generate_json": generate_json,
+            "generate_json_report": generate_json_report,
+            "generate_xml": generate_xml,
+            "generate_xml_report": generate_xml_report,
+            # execute
+            "execute_action": self.execute_action,
+            "execute_files": self.execute_files,
+            "add_package_to_executor": package_manager.add_package_to_executor,
+            # mock
+            "flask_mock_server_add_router": flask_mock_server_instance.add_router,
+            "start_flask_mock_server": flask_mock_server_instance.start_mock_server,
+        }
         # get all builtin function and add to event dict
         for function in getmembers(builtins, isbuiltin):
             self.event_dict.update({str(function[0]): function[1]})
